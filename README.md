de945233255688b1ae68d08ba4980262e9a997c3
Cristian Muscalu <cristian.muscalu@mindgeek.com>
2019-02-26 15:21:03 +0200

    added push notifications

--- README.md
+++ README.md
@@ -1,5 +1,29 @@
-PWA Test circleCI, codeclimate, heroku
+### PWA Test circleCI, codeclimate, heroku
 
-followed tutorial:  https://medium.freecodecamp.org/how-to-set-up-continuous-integration-and-deployment-for-your-react-app-d09ae4525250
+* followed tutorial:  https://medium.freecodecamp.org/how-to-set-up-continuous-integration-and-deployment-for-your-react-app-d09ae4525250
 
-deployed to : https://sleepy-beyond-83621.herokuapp.com/
+* deployed to : https://sleepy-beyond-83621.herokuapp.com/
+
+
+* send notification using postman:
+* post to : https://fcm.googleapis.com/fcm/send
+
+headers: 
+
+```
+Content-Type / application/json
+Authorization / key={token}
+```
+
+body - raw: 
+
+```javascript
+{
+    "notification": {
+        "title": "push notification title",
+        "body": "push notification body",
+        "click_action": "http://localhost:3000/"
+    },
+    "to": "{token}"
+}
+```
--- src/App.js
+++ src/App.js
@@ -1,17 +1,61 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+import firebase from "firebase/app";
+import NotificationAlert from "react-notification-alert";
+import "firebase/messaging";
+import "react-notification-alert/dist/animate.css";
 
 class App extends Component {
+  state = {
+    title: "",
+    body: ""
+  };
+
+  requestPermission = async () => {
+    try {
+      const messaging = firebase.messaging();
+      await messaging.requestPermission();
+      const token = await messaging.getToken();
+      console.log("token:", token);
+      return token;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  componentDidMount() {
+    this.requestPermission();
+  }
+
+  receivedMessage = notification => {
+    this.setState({ title: notification.title, body: notification.body });
+  };
+
+  showNotification = (place, type, message) => {
+    // primary, success, dandger, warning, info
+    const options = {
+      place,
+      message: <div>{message}</div>,
+      type,
+      icon: "tim-icons icon-bell-55",
+      autoDismiss: 5
+    };
+    this.refs.notificationAlert.notificationAlert(options);
+  };
+
   render() {
+    console.log(this.state);
+
+    const messaging = firebase.messaging();
+    messaging.onMessage(payload => {
+      console.log("Message received. ", payload.notification);
+      this.showNotification('bc', 'success', payload.notification.title);
+    });
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Github + Bitbucket commit on both
-          </p>
-        </header>
+        App - {this.state.title}
+        <NotificationAlert ref="notificationAlert" />
       </div>
     );
   }
